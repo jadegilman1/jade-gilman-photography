a0935717f2e17601b53e9d91c7b25f2d2a470340
jade gilman <jadegilman1@maine.edu>
2025-03-24 00:26:58 -0400

    test

--- utils/handler.py
+++ utils/handler.py
@@ -7,17 +7,27 @@ def run_script(script_path):
     except subprocess.CalledProcessError as e:
         print(f"Error running {script_path}:\n{e}")
 
+def git_commit_changes():
+    try:
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", "Automated commit of changes made by handler.py"], check=True)
+        print("Changes committed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error committing changes:\n{e}")
+
 if __name__ == "__main__":
     idiot_check = input("Please confirm you have uploaded the full resolution gallery to the file hosting site (github) prior to running this script. \nYour images are about to be irreversably compressed for website hosting. (yes/no): ")
     if idiot_check.lower() != 'yes':
         print("Make good decisions.")
         exit()
     scripts = [
-        'utils/img_resizer.py',
-        'utils/gallery_generator.py',
-        'utils/manifest_generator.py',
-        'utils/secrets_setter.py'
+        'img_resizer.py',
+        'gallery_generator.py',
+        'manifest_generator.py',
+        'secrets_setter.py'
     ]
 
     for script in scripts:
         run_script(script)
+
+    git_commit_changes()
