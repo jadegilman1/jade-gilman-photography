73699b2746fa42faf27711547c9eb1bbcf682046
Solanaceae <89823371+0xSolanaceae@users.noreply.github.com>
2026-02-06 22:28:05 -0500

    rm ps1

--- utils/add_gallery.ps1
+++
@@ -1,8 +0,0 @@
-# Navigate to project root (parent of utils/)
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$ProjectRoot = Split-Path -Parent $ScriptDir
-Set-Location $ProjectRoot
-
-python utils/handler.py
-
-Read-Host -Prompt "Press Enter to continue"
